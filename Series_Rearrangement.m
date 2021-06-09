6f09c130d96c116c60d309406009c58eeefb2e64
Narayan Jee Jha <58243776+9mpd@users.noreply.github.com>
2021-06-09 12:25:24 +0530

    Update Series_Rearrangement.m
    License Added

--- Series_Rearrangement.m
+++ Series_Rearrangement.m
@@ -1,3 +1,9 @@
+Copyright (c) 2021, 9mpd
+All rights reserved.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+
 %% Convergence of a conditionally convergent series at a desired value.
 %% Also calculating the time of execution.
 
